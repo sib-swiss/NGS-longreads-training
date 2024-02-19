d2725e37010aa99a500fb7b3edbacac202195f93
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2024-02-19 17:25:46 +0100

    updates project3 commands

--- scripts/project3_commands.sh
+++ scripts/project3_commands.sh
@@ -6,34 +6,26 @@
 # for issues with prokka:
 # cpanm Bio::SearchIO::hmmer --force
 
+cd ~/workdir/workdir/project3
+
 # bash function for performing the assembly and annotation
 assembly_annotation () {
     SAMPLE=$1
-    READTYPE=$2
+    
+    mkdir -p "$SAMPLE"_assembly
 
-    cd /home/jovyan/workdir/groupwork_assembly/"$SAMPLE"
-    
-    # if the readtype is HiFi take --pacbio-hifi as option to flye
-    # if the readtype is something else (i.e. CLR) take --pacbio-raw as option to flye
-    if [ $READTYPE == "HiFi" ]
-    then
-        MODE="--pacbio-hifi"
-    else
-        MODE="--pacbio-raw"
-    fi
-    
     # run flye with default options
     flye \
-    "$MODE" "$READTYPE"/reads/"$SAMPLE"_"$READTYPE".fasta.gz \
+    --pacbio-hifi "$SAMPLE".fastq.gz \
     --threads 4 \
-    --out-dir "$READTYPE"/assembly
+    --out-dir "$SAMPLE"_assembly
 
     # run BUSCO with auto lineage detection for prokaryotes (--auto-lineage-prok)
     # mode for genome and force overwrite (for re-running)
     busco \
-    -i "$READTYPE"/assembly/assembly.fasta \
+    -i "$SAMPLE"_assembly/assembly.fasta \
     --auto-lineage-prok \
-    --out_path "$READTYPE"/ \
+    --out_path "$SAMPLE"_assembly \
     --out BUSCO \
     --mode genome \
     --cpu 4 \
@@ -41,16 +33,15 @@ assembly_annotation () {
 
     # generate the BUSCO plot
     generate_plot.py \
-    -wd "$READTYPE"/BUSCO
+    -wd "$SAMPLE"_assembly/BUSCO
 
     # run prokka for annotation
     prokka \
     --cpus 4 \
-    --outdir "$READTYPE"/prokka \
-    "$READTYPE"/assembly/assembly.fasta
+    --outdir "$SAMPLE"_assembly/prokka \
+    "$SAMPLE"_assembly/assembly.fasta
 }
 
 # run the functions. Here an example for LWH7 on HiFi reads
-assembly_annotation LWH7 HiFi
-# and CLR reads:
-assembly_annotation LWH7 CLR
+assembly_annotation sample_1
+
